f62a01ab4234cac7974a0b386ad961ca4420c4ca
Erbynn John <john.erbynn@gmail.com>
2024-04-08 22:05:36 -0500

    add migrations

---
+++ src/StatlerWaldorfCorp.LocationService/Migrations/20240409030511_initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StatlerWaldorfCorp.LocationService.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:uuid-ossp", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "LocationRecords",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Latitude = table.Column<float>(type: "real", nullable: false),
+                    Longitude = table.Column<float>(type: "real", nullable: false),
+                    Altitude = table.Column<float>(type: "real", nullable: false),
+                    Timestamp = table.Column<long>(type: "bigint", nullable: false),
+                    MemberId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocationRecords", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LocationRecords");
+        }
+    }
+}
